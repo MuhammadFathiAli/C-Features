3544328c8793342f41ac7b7deeaffc249d176154
MuhammadFathiAli <muhammad.fathi64@gmail.com>
2023-06-29 22:25:40 +0300

    Add project files.

---
+++ Matching Patterns/IsMatchingPattern.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+User user = new User("Ahmed", subscription.silver);
+if (user != null && user.Name == "Ahmed" && user.Subscription == subscription.silver)
+{
+    Console.WriteLine(user);
+}
+else
+{
+    Console.WriteLine("Wrong");
+}
+// But You Can Use Pattern Matching
+/* Pattern Matching
+ * it lets you check if an object has specific charachetristics:
+    - is null or is not null => asking with "is" takes care of null check
+    - is of a particular type
+    - property has a specific value
+    * 
+ * ther's an overhead of it, so don't use in the hot path in ur code
+ */
+
+//user = null;
+//user.Subscription = null;
+if (user is { Name: "Ahmed", Subscription: subscription.silver })
+{
+    Console.WriteLine(user);
+}
+else
+{
+    Console.WriteLine("Wrong");
+}
+
+class User
+{
+    public string Name { get; set; }
+    public Subscription Subscription { get; set; }
+    public User(string name, Subscription subscription)
+    {
+        Name = name;
+        Subscription = subscription;
+    }
+    override ToString()
+    {
+        return $" User Name is {Name}, Subscription is {Subscription}";
+    }
+
+}
+enum Subscription
+{
+    regular, prime, silver, gold, premuim
+}
